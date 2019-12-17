a91922af2dd73f5788e4fc6b3d5f41729c54c579
Bonde005 <Bonde005@gmail.com>
2019-12-17 15:10:42 +0100

    h5t
    h4yht6

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width" , initial-scale=1.0 />
         <title>Læringsportofolie forside</title>
-        <link href="https://fonts.googleapis.com/css?family=Montserrat|PT+Sans|Quicksand&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|PT+Sans|Quicksand:400,600,700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="stylesheet_frontpage.css">
     </head>
     <body>
