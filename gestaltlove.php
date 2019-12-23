c7d52aae177bcd73853b5ea6bce26d8054a69bc2
Bonde005 <agne4531@student.eadania.dk>
2019-12-23 17:16:11 +0100

    gre
    gerg

---
+++ gestaltlove.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" , initial-scale=1.0 />
+        <title>AB læringsportfolio Gestaltlove</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|PT+Sans|Quicksand:400,600,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="stylesheet_page.css">
+        <link rel="stylesheet" href="grid_gestaltlove.css">
+    </head>
+    <body>
+
+      <?php include("header.inc"); ?>
+
+      <div id="pagewrap">
+        <div id="back_arrow">
+          <img src="billeder/back_arrow.png" alt="Tilbage pil" onclick="goBack()">
+        </div> <!-- back_arrow -->
+
+        <section id="overskrift">
+          <h1>Gestaltlove</h1>
+        </section> <!-- overskrift -->
+
+        <section id="indledning" class="tekstfelt">
+          <p>Når man opsætter et website, skal brugeren nemt kunne overskue siden og navigere rundt i det. Man skal altså være opmærksom på hvordan man opstiller indholdet på siden, så det er nemt at finde ud af hvad der hænger sammen og hvad der ikke gør. Når mennesker møde en hjemmeside eller et maleri, ses der på det hele før de enkelte elementer. Denne psykologi har lagt grundlag til de fem gestaltlove, som kan bruges sim hjælp til opsætningen af en website eller andet grafisk design. </p>
+          <p class="kilde">(Interfacedesign, s. 122)</p>
+          <section id="lokal_menu">
+            <p>Gestaltlovene indeholder fem love som er vigtigt at have kendskab til:</p>
+            <a href="#proximity_overskrift"><h2>Nærhed</h2></a>
+            <a href="#lighed_overskrift"><h2>Lighed</h2></a>
+            <a href="#kontinuitet_overskrift"><h2>Kontinuitet</h2></a>
+            <a href="#lukkethed_overskrift"><h2>Lukkethed</h2></a>
+            <p class="kilde">Interfacedesign, s. 123</p>
+          </section>
+        </section> <!-- indledning -->
+
+        <section id="proximity" class="boks">
+          <section id="proximity_overskrift" class="under_overskrift">
+            <h2>Nærhed</h2>
+          </section> <!-- proximity_overskrift -->
+
+          <section id="proximity_tekst" class="tekstfelt">
+            <p>Elementer som er tæt på hinanden, bliver opfattet som værende relateret. Den engelske betegnelse er proximity.</p>
+            <p class="kilde">(Interfacedesign s. 122)</p>
+          </section> <!-- proximity_tekst -->
+
+          <section id="proximity_eksempel" class="illustrationer">
+            <img src="billeder/grafisk_design/gestalt_proximity.png" alt="Eksempel af gestaltloven om nærhed">
+          </section> <!-- proximity_eksempel -->
+        </section> <!-- proximity boks  -->
+
+        <section id="lighed" class="boks">
+          <section id="lighed_overskrift" class="under_overskrift">
+            <h2>Lighed</h2>
+          </section> <!-- lighed_overskrift -->
+
+          <section id="lighed_tekst" class="tekstfelt">
+            <p>Elementer der har ser ens ud, f.eks. samme farve, form eller størrelse, betragtes som at høre sammen. Den engelske betegnelse er similarity.</p>
+            <p class="kilde">(Interfacedesign s. 122)</p>
+          </section> <!-- lighed_tekst -->
+
+          <section id="lighed_eksempel" class="illustrationer">
+            <img src="billeder/grafisk_design/gestalt_lighed.png" alt="Eksemel af gestaltloven om lighed">
+          </section> <!-- lighed_eksempel -->
+        </section> <!-- lighed boks  -->
+
+        <section id="kontinuitet" class="boks">
+          <section id="kontinuitet_overskrift" class="under_overskrift">
+            <h2>Kontinuitet</h2>
+          </section> <!-- kontinuitet_overskrift -->
+
+          <section id="kontinuitet_tekst" class="tekstfelt">
+            <p>Elementer der er placeret således at de danne en linje eller kurve, bliver anset som at være relateret til hinanden. Den engelske betegnelse er good continuation.</p>
+            <p class="kilde">(Interfacedesign s. 122)</p>
+          </section> <!-- kontinuitet_tekst -->
+
+          <section id="kontinuitet_eksempel" class="illustrationer">
+            <img src="billeder/grafisk_design/gestalt_kontinuitet.png" alt="Eksemel af gestaltloven om kontinuitet">
+          </section> <!-- kontinuitet_eksempel -->
+        </section> <!-- kontinuitet boks  -->
+
+        <section id="lukkethed" class="boks">
+          <section id="lukkethed_overskrift" class="under_overskrift">
+            <h2>Lukkethed</h2>
+          </section> <!-- lukkethed_overskrift -->
+
+          <section id="lukkethed_tekst" class="tekstfelt">
+            <p>Elementer som er placeret i samme ramme, ”lukket” inde sammen, bliver betragtet som relateret. Den engelske betegnelse er common region.</p>
+            <p class="kilde">(Interfacedesign s. 122)</p>
+          </section> <!-- lukkethed_tekst -->
+
+          <section id="kontinuitet_eksempel" class="illustrationer">
+            <img src="billeder/grafisk_design/gestalt_lukkethed.png" alt="Eksemel af gestaltloven om lukkethed">
+          </section> <!-- lukkethed_eksempel -->
+        </section> <!-- lukkethed boks  -->
+
+        <section id="figurdannelse" class="boks">
+          <section id="figurdannelse_overskrift" class="under_overskrift">
+            <h2>Figurdannelse</h2>
+          </section> <!-- figurdannelse_overskrift -->
+
+          <section id="figurdannelse_tekst" class="tekstfelt">
+            <p>Elementer som er hver for sig kan opfattes som en samlet figur, også selvom figuren reelt ikke er eksplicit til stedet. Den engelske betegnelse er closure.</p>
+            <p class="kilde">(Interfacedesign s. 122)</p>
+          </section> <!-- figurdannelse_tekst -->
+
+          <section id="figurdannelse_eksempel" class="illustrationer">
+            <img src="billeder/grafisk_design/gestalt_figurdannelse.png" alt="Eksemel af gestaltloven om figurdannelse">
+          </section> <!-- figurdannelse_eksempel -->
+        </section> <!-- figurdannelse boks  -->
+      </div> <!-- pagewrap -->
+
+      <div id="home_button">
+        <a href="index.php"><img src="billeder/home.png" alt="home knap"></a>
+      </div>
+
+      <?php include("footer.inc"); ?>
+    </body>
+    <script>
+      function goBack() { /* Går en tilbage i historikken */
+      window.history.back();
+      }
+    </script>
+  </html>
---
+++ illustrator.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" , initial-scale=1.0 />
+        <title>AB læringsportfolio Illustrator</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|PT+Sans|Quicksand:400,600,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="stylesheet_page.css">
+        <link rel="stylesheet" href="grid_illustrator.css">
+        <link rel="stylesheet" href="slideshow.css">
+    </head>
+    <body>
+
+      <?php include("header.inc"); ?>
+
+      <div id="pagewrap">
+        <div id="back_arrow">
+          <img src="billeder/back_arrow.png" alt="Tilbage pil" onclick="goBack()">
+        </div> <!-- back_arrow -->
+
+        <section id="overskrift">
+          <h1>Illustrator</h1>
+        </section> <!-- overskrift -->
+
+        <section id="indledning" class="tekstfelt">
+          <p>Adobes program Illustrator bruges for det meste til logoer og ikoner. Programmet benytter sig af vektoren, som man kender dem fra matematikkens verden. Dvs. at der ikke anvendes pixels som i de fleste redigeringsprogrammer. Fordelen ved dette er at man kan skalere sit arbejde i Illustrator uden at der sker ændringer. Hvis man f.eks. gør et pixeleret arbejde meget større, bliver de individuelle pixels forstørret, og det medvirker et uklart produkt hvor man tydeligt kan se at det består af firkanter. Hvis man derimod forstørre et vektorprodukt undgår man dette problem. Derfor er Illustrator god til at designe logoer og ikoner, da de ofte skal bruges i forskellige størrelser til forskellige medier og andre produkter. Derved er der ingen problem ved skaleringen.</p>
+          <p class="kilde">(Carsten Kjeldsen Bogner, Illustrator: grundlæggende)</p>
+        </section> <!-- indledning -->
+
+        <section id="funktioner_overskrift" class="under_overskrift">
+          <h2>Funktioner</h2>
+        </section> <!-- funktioner_overskrift -->
+
+        <section id="funktioner_tekst" class="tekstfelt">
+          <p>Der er nogle væsentlige funktioner i Illustrator, som er godt at kende til når man starter ud. Når man arbejder i Illustrator arbejder man i lag, så man nemt kan adskille og redigere i objekter. Derved får man også et bedre overblik over sit arbejde, især hvis man arbejder med noget kompleks der indgår en masse objekter, eller hvis man arbejder på mange forskellige produkter i samme fil. Det er nemlig muligt i Illustrator at arbejde på flere produkter i samme fil på en overskuelig måde. Det skyldes funktionen kaldet artboards. I sin fil hvor man arbejder, har man flere ”stykker papir”, hvor man kan arbejde derpå. Hvis man eksporter filen til f.eks. en pdf-fil, bliver det man har lavet på de individuelle artboards gemt på deres egne sider. De bliver altså ikke sat sammen til ét produkt. Derudover er det kun de selve objekter der befinder sig på artboardet og ikke dem udenfor. Det er ligesom at have et arbejdsbord med flere stykker papir på, hvor man kan lægge ting på papirerne og rykke dem ud på arbejdsbordet som man vil. Man kan altså godt flytte objekter væk fra artboardet i stedet for at slette dem, hvis man gerne vil gemme på dem. Udover disse to funktioner findes der mange flere i programmet som man kan beskæftige sig kendskab til.</p>
+          <p class="kilde">(Carsten Kjeldsen Bogner, Illustrator: grundlæggende)</p>
+        </section> <!-- funktioner_tekst -->
+
+        <section id="artboards_eksempler" class="illustrationer">
+          <img src="billeder/grafisk_design/artboards_lag.png" alt="Eksempler af artboards og lag">
+          <blockquote>
+            <p>Artboarda og lag i Illustrator</p>
+          </blockquote>
+        </section> <!-- artboards_eksempler -->
+
+        <section id="former_tekst" class="tekstfelt">
+          <p>Det er dog vigtigt at tænke på at Illustrator ikke er et frihåndsprogram, og det vil være en fordel at tænke i former. Når man vil lave et ikon, kan man komme frem til den ønskede form ved at kombinere andre former, og slette det overskydende. Man kan også bruge andre funktioner til at manipulere vektorerne, og derved få den form man ønsker. Når man har alt det på plads, er det bare at udforske programmet selv.</p>
+          <p class="kilde">(Carsten Kjeldsen Bogner, Illustrator: grundlæggende)</p>
+        </section> <!-- former_tekst -->
+
+        <section id="element_tekst" class="tekstfelt">
+          <p>Selve indholdet i en html-fil består af elementer. Elementerne består af et start tag og et slut tag, som indikere starten og slutningen på elementet. Taget kan have en attribut som har en værdi, f.eks. hvis en img tag har en class attribut med værdien ”billeder”. Hvis taget har en attribut, indikeres det i start taget. Selve indholdet findes imellem tagsene. </p>
+          <p class="kilde">(Niels Østergaard, introduktion til HTML)</p>
+        </section> <!-- element_tekst -->
+
+        <section id="element_billede" class="illustrationer">
+          <img src="billeder/websites/html/element.png" alt="et element">
+          <blockquote>
+            <p>Illustration af opbygningen af et element</p>
+            <a class="kilde" href="http://langintro.com/cljsbook/_images/terminology.png"><p>langintro.com</p></a>
+          </blockquote>
+        </section> <!-- element_billede -->
+
+        <section id="kodeskik_overskrift" class="under_overskrift">
+          <h2>Kodeskik</h2>
+        </section> <!-- kodeskik_overskrift -->
+
+        <section id="kodeskik_tekst" class="tekstfelt">
+          <p>For at nemt kunne finde rundt i sin kode, og for at andre nemt kan finde rundt i ens kode, skal man følge godt kodeskik. Heriblandt skal man sørge for at lave indryk i sin kode således at man nemt kan se hvad der høre sammen med hvad, f.eks. kan man angive indholdet i en sektion ved at lave et indryk så alt indholdet er rykket ind i sektionen. Derudover er det også godt at give sine sektioner, eller andre tags hvor det er relevante, gode ider eller classes så man nemt kan se og forstå hvad indholdet er og hvad det skal bruges til. Man skal ikke bruge sit egen forkortelsessystem til at navngive sit indhold men derimod bruge navne der kan forstå af en der slet ikke har kigget på koden før. Ligeledes er det en del af god kodeskik at bruge kommentar. Man kan bruge kommentar til at give en indblik i hvad de forskellige dele skal bruges til og er i sær også god til at vise hvor de forskellige tags slutter. Hvis man f.eks. har en lang sektion med meget indhold i, vil det blive nemmere overskueligt at se hvor sektionen slutter hvis man tilføjer en kommentar ved slut tagget. </p>
+          <p class="kilde">(Niels Østergaard, introduktion til HTML) </p>
+        </section> <!-- kodeskik_tekst -->
+
+        <section id="kodeskik_billede" class="illustrationer">
+          <img src="billeder/websites/html/kodeskik.png" alt="eksempel på god kodeskik">
+          <blockquote>
+            <p>Et eksempel på god kodeskik med indryk og kommentar</p>
+          </blockquote>
+        </section>
+      </div> <!-- pagewrap -->
+
+      <div id="home_button">
+        <a href="index.php"><img src="billeder/home.png" alt="home knap"></a>
+      </div>
+
+      <section id="slideshow">
+        <div class="slideshow-container">
+          <div class="mySlides fade">
+            <div class="numbertext">1 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/captain_america.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">2 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/celtic_cross.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">3 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/celtic_figure.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">4 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/EU.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">5 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/hund.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">6 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/illustrator_c.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">7 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/illustrator_c_gray.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">8 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/illustrator_logo_batman.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">9 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/illustrator_owl.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">10 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/illustrator_typography.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">11 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/illustrator_weather_icon.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">12 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/logo.png">
+          </div> <!-- mySlides fade -->
+
+          <div class="mySlides fade">
+            <div class="numbertext">13 / 13</div>
+            <img src="billeder/grafisk_design/slideshow_illustrator/sofa.png">
+          </div> <!-- mySlides fade -->
+
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+      </div> <!-- slideshow-contaioner -->
+      <br> <!-- linebreak -->
+
+      <div id="prikker"> <!-- style="text-align:center" -->
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(4)"></span>
+        <span class="dot" onclick="currentSlide(5)"></span>
+        <span class="dot" onclick="currentSlide(6)"></span>
+        <span class="dot" onclick="currentSlide(7)"></span>
+        <span class="dot" onclick="currentSlide(8)"></span>
+        <span class="dot" onclick="currentSlide(9)"></span>
+        <span class="dot" onclick="currentSlide(10)"></span>
+        <span class="dot" onclick="currentSlide(11)"></span>
+        <span class="dot" onclick="currentSlide(12)"></span>
+        <span class="dot" onclick="currentSlide(13)"></span>
+      </div> <!-- prikker -->
+    </section> <!-- slideshow -->
+
+      <?php include("footer.inc"); ?>
+    </body> <!-- pagewrap -->
+    <script>
+      function goBack() { /* Går en tilbage i historikken */
+      window.history.back();
+      }
+
+      var slideIndex = 1;
+      showSlides(slideIndex);
+
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
+
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+      }
+    </script>
+  </html>
