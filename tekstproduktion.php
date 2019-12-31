c41e63b433e2cf3888db3f22f70d9973a6692fbb
Bonde005 <agne4531@student.eadania.dk>
2019-12-31 16:35:44 +0100

    dsf
    sfd

---
+++ tekstproduktion.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" , initial-scale=1.0 />
+        <title>AB læringsportfolio Tekstproduktion</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|PT+Sans|Quicksand:400,600,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="stylesheet_page.css">
+        <link rel="stylesheet" href="grid_tekstproduktion.css">
+    </head>
+    <body>
+
+      <?php include("header.inc"); ?>
+
+      <div id="pagewrap">
+        <div id="back_arrow">
+          <img src="billeder/back_arrow.png" alt="Tilbage pil" onclick="goBack()">
+        </div> <!-- back_arrow -->
+
+        <section id="overskrift">
+          <h1>Tekstproduktion</h1>
+        </section> <!-- overskrift -->
+
+        <section id="indledning" class="tekstfelt">
+          <p>Selve teksten opsættes forskelligt alt efter medietypen. Medietyper har hver især deres krav og måder at kommunikere på, og der skal teksten følge med. Der er altså forskellige måder at skrive til en artikel, brochure, annonce, web og sociale medier.</p>
+          <section id="lokal_menu">
+            <p>Punkter i dette emne</p>
+            <a href="#webtekster_overskrift"><h2>Webtekster</h2></a>
+            <a href="#sociale_medier_overskrift"><h2>Sociale medier</h2></a>
+            <a href="#annonce_overskrift"><h2>Annonce</h2></a>
+            <a href="#brochure_overskrift"><h2>Brochure</h2></a>
+            <a href="#artikel_overskrift"><h2>Artikel</h2></a>
+          </section>
+        </section> <!-- indledning -->
+
+        <section id="kodning_overskrift" class="under_overskrift">
+          <h2>Webtekster</h2>
+        </section> <!-- Webtekster_overskrift -->
+
+        <section id="webtekster_tekst" class="tekstfelt">
+          <p>Den måde man tilgår websites, er anderledes end tekst på papir. Med eye-tracking er man kommet frem til at folk kigger på websites som i en form af F. Først kigges der vandret i den øverste del af siden, dernæst kigger man lodret ned i siden, hvorefter der kigger vandret ind på siden igen dog kortere. Til sidst kiggers der blot lodret ned i venstre siden. På denne måde kan man forstå hvad læseren egentligt læser og hvor vigtige elementer skal placeres. Derfor skal det vigtige placeres i toppen af webteksten.</p>
+          <p class="kilde">Online Kommunikation, s. 191-193</p>
+        </section> <!-- webtekster_tekst -->
+
+        <section id="eyetracking_billede" class="illustrationer">
+          <img src="billeder/kommunikation/eyetracking.PNG" alt="Eyetracking på hjemmesider">
+          <blockquote>
+            <p>Første side er en "om os" side, anden side er en e-butik og sidste side er google søgeresultater</p>
+            <p class="kilde">Online kommunikation, s. 192</p>
+          </blockquote>
+        </section> <!-- eyetracking_billede -->
+
+        <section id="webtekster_forsat_tekst" class="tekstfelt">
+          <p>Derudover skal teksten formuleres klart og tydeligt, der skal ikke være nogen lang indledning og det skal være tydeligt for læseren hvor de forskellige informationer befinder sig. Dette er fordi brugeren næsten altid har travlt, er utålmodig og vil hurtigt videre. På samme måde skal man sørge for at benytte sig af links, for at føre brugeren videre. Disse links skal være klart og tydelig i hvor de føre hen. Man skal altså undgå links som ”klik her”. Der skal ikke være lange formuleringer men blot links og muligvis en kort tekst hvis nødvendigt. Man kan med fordel bruge modellen Hey You See So. Hvor man først opfanger opmærksomheden med en overskrift, fortæller derefter hvorfor det er relevant for personen, laver en kort uddybning og til sidst en mulighed for at en aktion.</p>
+          <p class="kilde">Online Kommunikation, s. 196-199</p>
+        </section> <!-- webtekster_forsat_tekst -->
+
+        <section id="webtekst_eksempel" class="illustrationer">
+          <img src="billeder/kommunikation/webtekst.PNG" alt="Forbedret webtekst">
+          <blockquote>
+            <p>Eksemepl af hvad man <i>ikke</i> skal gøre og hvordan man skal gøre det</p>
+            <p class="kilde">Online kommunikation, s. 199</p>
+          </blockquote>
+        </section>
+
+        <section id="sociale_medier_overskrift" class="under_overskrift">
+          <div class="tekst">
+            <p>Til sociale medietekster er der ikke faste regler og modeller, da de sociale medier er meget præget af spontanitet.</p>
+            <p class="kilde">(Morten Kluw Wøldike Schmith, Tekstproduktion)</p>
+            <p>Man vil altid søge efter meget interaktion på de sociale medier. Man kan derved benytte sig af billeder samt en caption der skal stille op til en interaktion. Det kan herved være f.eks. et spørgsmål som leder til kommentar. Det kan også være konkurrencer hvor man får folk til at dele, kommenter, følge og/eller like. Ved at skabe meget interaktionen på et opslag, kommer der ud til flere personer og de sociale medier algoritmer vil understøtte opslagene mere. Ved f.eks. Facebook skal man have i tankerne at de ikke vil hjælpe opslag der vil føre brugeren væk fra platformen. Det vil sige at hver gang man har links til en anden side, vil algoritmen ikke understøtte opslaget og det bliver svære at få det ud til flere. Hvis man derfor vil bruge videoer på Facebook, skal man slå selve videoen op og ikke et link til en på YouTube.</p>
+            <p class="kilde">(Fra SoMe til SoWe, Mia Ben-Ami)</p>
+          </div>
+          <div class="punkter">
+            <p>Derudover når man skal kommunikere direkte med folk i virksomhedssammenhæng, er der en model med 4 strategier som hver har tilknyttet en farve.</p>
+            <dl>
+              <dt>Blå</dt>
+                <dd>er symbolsk for information og facts. Fokuser på tal, datoer, præcision og information.</dd>
+              <dt>Grøn</dt>
+                <dd>er symbolsk for garantier og kvalitet. Fokuser på garantier, tradition, orden og kvalitet</dd>
+              <dt>Gul</dt>
+                <dd>er symbolsk for oplevelser. Fokuser p ideer, visioner, sjov og spontanitet og oplevelser.</dd>
+            </dl>
+            <p class="kilde">(Kommunikation i multimediedesign, s. 249-250)</p>
+          </div>
+        </section>
+
+        <section id="kommentar_billede" class="illustrationer">
+          <img src="billeder/kommunikation/socialemedier_dgp.PNG" alt="Interaktion på sociale medier">
+          <blockquote>
+            <p>Et eksempel på en rød og blå tilgang. Der gives information samtidig med sympati.</p>
+            <a class="kilde" href="https://www.facebook.com/pigespejder/"><p>De Grønne Pigespejdere facebookside</p></a>
+          </blockquote>
+        </section> <!-- kommentar_billede -->
+
+        <section id="annonce_overskrift" class="under_overskrift">
+          <h2>Annonce</h2>
+        </section> <!-- annonce_overskrift -->
+
+        <section id="annonce_tekst" class="tekstfelt">
+          <div class="tekst">
+            <p>En annonce skal være hurtig og præcis i sin kommunikation. Overskriften er her vigtigt da den skal skabe interesse og fange målgruppens opmærksomhed. Brødteksten i annoncen skal være kort og tydelig, hvor det basale kommer tydeligt frem. Her kan man med fordel benytte sig af AIDA modellen. Man skal først skabe opmærksomhed, derefter en interesse for produktet, efterfulgt af en længsel efter produktet som til sidst skal ende med an aktion hvor brugeren kan købe produktet. Attention fås i overskriften, hvor interessen og længslen fås i annoncens billeder og/eller brødtekst. Her skal budskabet være så klart at det skaber en interesse som bliver til en længsel. Aktion bliver frembragt af en call to action, som i dette tilfælde ofte er et firmanavn/logo eller hvor produktet kan købes. Teksten i annoncen kan blive udformet ved hjælp af afsæt i nogle strategier.</p>
+          </div> <!-- tekst -->
+          <div class="punkter">
+            <dl>
+              <dt>USP: Uniqe Selling Propostition</dt>
+                <dd>Går på tilbud eller det unikke</dd>
+              <dt>ESP: Emotional Selling Proposition</dt>
+                <dd>Spiller på følelse, kan både være gode følelser og alvorlige.</dd>
+              <dt>ISP: Ironic Selling Proposition</dt>
+                <dd>Går på ironi og sort humor</dd>
+            </dl>
+          </div> <!-- punkterne -->
+          <p class="kilde">(Kommunikation i multimediedesign, s. 244-246)</p>
+        </section> <!-- annonce_tekst -->
+
+        <section id="aida_billede" class="illustrationer">
+          <img src="billeder/kommunikation/aida.png" alt="AIDA modellen">
+          <blockquote>
+            <p>AIDA modellen</p>
+            <a class="kilde" href="http://www.karimelganayni.com/what-is-the-aida-model-in-advertising/"<p>karimelganayni.com</p></a>
+          </blockquote>
+        </section> <!-- aida_billede -->
+
+        <section id="brochure_overskrift" class="under_overskrift">
+          <h2>Brochure</h2>
+        </section> <!-- brochure_overskrift -->
+
+        <section id="brochure_tekst" class="tekstfelt">
+          <p>Her er det vigtigt at tænke på typografi og billeder i sammenhæng med tekstindholdet, for at de skal styrke hinanden og derved fremstå troværdig. Man skal have fundet sit overordnet formål for brochuren, samtidig med at være klar over hvem målgruppen er. Der skal også træffes valg for stilen og det visuelle design, hvilke følelser og opfattelser vil man give målgruppen. Med alt dette i tankerne kan man begynde at producere teksten. Forsiden af brochuren skal ”sælge drømmen”, her placere man ofte et interessevækkende billede samt logo. Side 2 og 3 skal indeholde billeder og informerende tekst om eks. Produktet men det skal også kunne appellere til kundernes følelser. Side 4 er bagsiden og her skal der være kontaktinformation og servicemuligheder. Her kan man også benytte sig af AIDA-modellen. Man skal altid tænke på at kort er godt, når det kommer til brochure.</p>
+          <p class="kilde">(Kommunikation i multimediedesign, s. 243-244)</p>
+        </section> <!-- brochure_tekst -->
+
+        <section id="brochure_billede" class="illustrationer">
+          <img src="billeder/kommunikation/brochure_eksempel.jpg" alt="En brochure">
+          <blockquote>
+            <p>Eksempel af en brochure</p>
+            <a class="kilde" href="https://www.uprinting.com/brochure-printing.html"><p>uprinting.com</p></a>
+          </blockquote>
+        </section> <!-- brochure_billede -->
+
+        <section id="artikel_overskrift" class="under_overskrift">
+          <h2>Artikel</h2>
+        </section> <!-- artikel_overskrift -->
+
+        <section id="artikel_tekst" class="under_overskrift">
+          <div class="tekst">
+            <p>Når man skriver en nyhedsartikel, skal man altid tænke på det vigtigste først. Man skal tænke på at læseren muligvis ikke vil læse hele artiklen og skal derfor have det vigtigste i den øverste del af brødteksten. Samtidig med at der skal fanges opmærksomhed og skabes interesse, vha. en rubrik, underrubrik, billede og manchet. Artiklen skal også have mellemrubrikker der hjælper læseren med at få et overblik.</p>
+            <p class="kilde">(kommunikation i multimediedesign, s. 240-241)</p>
+          </div> <!-- tekst -->
+          <div class="punkter">
+            <p>Ved en nyhedsartikel er der fem nyhedskriterier:</p>
+            <dl>
+              <dt>Væsentlighed</dt>
+                <dd>Noget der har betydning eller konsekvens for mange mennesker.</dd>
+              <dt>Sensation</dt>
+                <dd>Noget der fscinerer eller chokerer.</dd>
+              <dt>Konflikt</dt>
+                <dd>Når sager eller psersoner er på kollisionskurs.</dd>
+              <dt>Identifikation</dt>
+                <dd>Noget læseren kan genkende sig selv i.</dd>
+              <dt>Aktualitet</dt>
+                <dd>Noget der er oppe i tiden.</dd>
+            </dl>
+            <p class="kilde">(Morten Kluw Wøldike Schmith, Tekstproduktion)</p>
+          </div> <!-- punkter -->
+        </section> <!-- artikel_tekst -->
+
+        <section id="nyhedstrekant_billede" class="illustrationer">
+          <img src="billeder/kommunikation/nyhedstrekant.jpg" alt="Nyhedstrekant">
+          <blockquote>
+            <p>Nyhedstrekanten, hvor man skal have det vigtigste først</p>
+            <a class="kilde" href="https://ritzau.com/saadan-bruger-du-nyhedstrekanten/"><p>ritzau.com</p></a>
+          </blockquote>
+        </section> <!-- nyhedstrekant_billede -->
+
+        <section id="subjektiv_artikel_tekst" class="illustrationer">
+          <p>Ved subjektive artikel kan de blive struktureret anderledes. Her kan man benytte kommode-strukturen. Det består af en ramme og nogle skuffer. Rammen har en top og en bund som ligger fast, men skufferne kan man selv vælge at åbne og kigge i. En subjektiv artikel består altså af en begyndelse og en slutning, hvor derimellem er en række mindre delfortællinger.</p>
+          <p class="kilde">(Kommunikation i multimediedesign, s. 241)</p>
+        </section> <!-- subjektiv_artikel_tekst -->
+
+        <section id="kommodemodel_billede" class="illustrationer">
+          <img src="billeder/kommunikation/kommodemodel.jpg" alt="Kommodemodel">
+          <blockquote>
+            <p>Kommodemodellen som bruges til opbygning af subjektive artikler</p>
+            <a class="kilde" href="https://systimes.systime.dk/index.php?id=136"><p>systimes.systime.dk</p></a>
+          </blockquote>
+        </section> <!-- kommodemodel_billede -->
+      </div> <!-- pagewrap -->
+
+      <div id="home_button">
+        <a href="index.php"><img src="billeder/home.png" alt="home knap"></a>
+      </div>
+
+      <?php include("footer.inc"); ?>
+    </body> <!-- pagewrap -->
+    <script>
+      function goBack() { /* Går en tilbage i historikken */
+      window.history.back();
+      }
+    </script>
+  </html>
