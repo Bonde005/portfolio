0547c08071a87dd37520d7f44e6c0623400cd11c
Bonde005 <Bonde005@gmail.com>
2020-01-03 13:27:19 +0100

    herh
    hrehre

---
+++ design_thinking.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" , initial-scale=1.0 />
+        <title>AB læringsportfolio Design Thinking</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|PT+Sans|Quicksand:400,600,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="stylesheet_page.css">
+        <link rel="stylesheet" href="grid_design_thinking.css">
+    </head>
+    <body>
+
+      <?php include("header.inc"); ?>
+
+      <div id="pagewrap">
+        <div id="back_arrow">
+          <img src="billeder/back_arrow.png" alt="Tilbage pil" onclick="goBack()">
+        </div> <!-- back_arrow -->
+
+        <section id="overskrift">
+          <h1>Design Thinking</h1>
+        </section> <!-- overskrift -->
+
+        <section id="indledning" class="tekstfelt">
+          <p>Design thinking er en metode der bruges til at opnå innovative resultater, det kan både være produkter og services. Det er en meget benyttet metode, som er god til at sørge for at man forbliver innovativ og ikke sætter sig fast i en tanke eller ide. Metoden fungerer ved at man kommer igennem 6 faser. Man kan tage tilbage i faserne og på den måde behøver det ikke at være lineært.</p>
+          <p class="kilde">(Niels Østergaard, Intro til Design Thinking)</p>
+          <section id="lokal_menu">
+            <p>Punkter i dette emne</p>
+            <a href="#faserne_overskrift"><h2>Faserne</h2></a>
+            <a href="#projekt_overskrift"><h2>Projekt</h2></a>
+          </section>
+        </section> <!-- indledning -->
+
+        <section id="faserne_overskrift" class="under_overskrift">
+          <h2>Faserne</h2>
+        </section> <!-- faserne_overskrift -->
+
+        <section id="faserne_tekst" class="tekstfelt">
+          <p>Den første fase hedder empathize. Her gælder det om at være nysgerrigt, og opsøge så meget viden indenfor emnet som muligt. Ved at gøre dette kan man finde viden man slet ikke vidste der var der før. Her er det ikke kun research i form af tekster men også observationer og interviews. Den næste fase hedder define, her skal man til at kigge på alt den viden man har opnået, og herved finder et problem eller en vinkel. Hertil kan man lave en framing som man arbejder udefter efterfølgende. Det kan være at man i empathize fasen har fundet ud af at folk mister deres nøgler ofte, og dertil vil man define en vinkel/problem, hvor man vil hjælpe folk med at vide hvor deres nøgler er henne. Den næste fase er ideate, her skal man til at udføre en idégeneration. Her skal man komme med så mange idéer som muligt, og man må ikke sætte sig fast på en endnu. Der er ikke nogen dårlige idéer og skal derfor ikke binde sig til at være realistisk. Ved at tænke divergent får man ny inspiration. Først til sidst må man være konvergerende og sortere de urealistiske idéer fra. Her vælger man en idé at arbejde videre med. Næste fase er prototype. Her skal man til at bygge en prototype som man kan få testet. Den første prototype skal ikke være detaljeret, blot at man kan få videreført idéen. Til dette kan man benytte sig af papirprototyper, hvor man skitsere idéen. Næste fase er test. Ved at testet prototypen kan man undgå problemer og komme med nye idéer til at forbedre produktet, derved kan det være oplagt at gå tilbage til prototypefasen og lave en ny prototype som skal testes. Fasen herefter er implement. Når man er tilfreds med sit produkt og det fungerer ordentligt, eller hvis en deadline er tæt på, skal man til at implementere sin idé i virkeligheden. Dette kan være noget der skal implementeres til et eksisterende produkt, men kan også være at producere produktet og gør det tilgængeligt.</p>
+          <p class="kilde">(Niels Østergaard, Intro til Design Thinking)</p>
+        </section> <!-- faserne_tekst -->
+
+        <section id="faserne_billede" class="illustrationer">
+          <img src="billeder/projekter_opgaver/faser.png" alt="Faserne i Design Thinking">
+          <blockquote>
+            <p>Faserne i Design Thinking og hvordan man bevæger sig mellem dem</p>
+            <a class="kilde" href="https://gist.github.com/w33ble/46ce7bb221c30a8962e3db4cdc2afe94"><p>gist.github.com</p></a>
+          </blockquote>
+        </section> <!-- faserne_billede -->
+
+        <section id="Projekt_overskrift" class="under_overskrift">
+          <h2>Projekt</h2>
+        </section> <!-- projekt_overskrift -->
+
+        <section id="projekt_tekst" class="tekstfelt">
+          <p>På holdet blev vi inddelt i grupper af tre og to med fire, hvor jeg befandt mig i en tremandsgruppe. Her var emnet for projektet udlån og reservation. Til at starte med brainstormede vi emner under udlån og reservation, hvorefter vi udvalgte nogle at kigge videre på, hvor der blev valgt emnet camping. Dertil lavede vi endnu en brainstorm om hvad emnet indeholdte med relevans til udlån og reservation. Derefter kontaktede vi to forskellige campister og en ejer af en campingplads. Vi ville gerne også have kontaktet en ny campist, som ikke havde camperede så meget endnu, men vi kunne ikke nå at få fat i en. En video over hvad vi fik ud af empatize fasen kan findes her. I define fasen kiggede vi på den viden vi havde fået fra den foregående fase, og derudfra fandt vi flere mulige problemstillinger af arbejde videre med. Her valgte vil at arbejde videre med at tiltrække unge til at campere. En video over hvad vi har fået ud define fasen kan ses her. Dernæst begyndte vi at brainstorme idéer til produktet, som en del af opgaven var at det skulle være en app. Vi kom med idéer til hvad appen skulle indeholde og hvordan den skulle tiltrække unge. Vi valgte en idé at gå videre med, hvor vi begyndte at lave en papirprototype af appen. Efter af prototypen var lavet gik vi videre til næste fase, test. Her gik vi rundt på campus og testede prototypen ikke kun på andre multimediedesignere men også fra andre uddannelser, som der derfor ikke havde nogen baggrundsviden om projektet. Efter testene blev der hurtigt lavede nogle små ændringer til prototypen, og andre observationer derfra forblev blot observationer vi kunne have med videre. Dog havde vi ikke tid til at arbejde mere med appen.</p>
+          <p class="kilde">(Niels Østergaard, introduktion til HTML) </p>
+        </section> <!-- kodeskik_tekst -->
+
+        <section id="projekt_pitch" class="video">
+          <!-- <iframe src="https://www.youtube.com/embedF6xGflGO6CI/"></iframe> -->
+          <iframe width="420" height="315"
+src="https://www.youtube.com/embed/tgbNymZ7vqY">
+</iframe> 
+        </section>
+
+      <div id="home_button">
+        <a href="index.php"><img src="billeder/home.png" alt="home knap"></a>
+      </div>
+
+      <?php include("footer.inc"); ?>
+    </body> <!-- pagewrap -->
+    <script>
+      function goBack() { /* Går en tilbage i historikken */
+      window.history.back();
+      }
+    </script>
+  </html>
