5c763745ef12cce7d6672d769bc2ad00a6278568
Bonde005 <Bonde005@gmail.com>
2020-01-06 13:25:07 +0100

    ye
    ye

---
+++ bag_om_siden.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" , initial-scale=1.0 />
+        <title>AB læringsportfolio Bag om siden</title>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|PT+Sans|Quicksand:400,600,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="stylesheet_page.css">
+        <link rel="stylesheet" href="grid_bag_om_siden.css">
+    </head>
+    <body>
+
+      <?php include("header.inc"); ?>
+
+      <div id="pagewrap">
+        <div id="back_arrow">
+          <img src="billeder/back_arrow.png" alt="Tilbage pil" onclick="goBack()">
+        </div> <!-- back_arrow -->
+
+        <section id="overskrift">
+          <h1>Bag om siden</h1>
+        </section> <!-- overskrift -->
+
+        <section id="indledning" class="tekstfelt">
+          <p>Dette website er et projekt i sig selv, og derfor er jeg gået igennem mange faser og gjort mange overvejelser. På denne side vil jeg uddybe lidt om mit arbejde igennem projektet og hvilke overvejelser jeg har gjort mig i forbindelse med design og struktur, samt reflektere over siden og projektet.</p>
+          <section id="lokal_menu">
+            <p>Punkter i dette emne</p>
+            <a href="#start_overskrift"><h2>Start af projektet</h2></a>
+            <a href="#struktur_overskrift"><h2>Struktur</h2></a>
+            <a href="#design_overskrift"><h2>Design</h2></a>
+            <a href="#refleksion_overskrift"><h2>Refleksion</h2></a>
+          </section>
+        </section> <!-- indledning -->
+
+        <section id="start_overskrift" class="under_overskrift">
+          <h2>Start af projektet</h2>
+        </section> <!-- kodning_overskrift -->
+
+        <section id="start_tekst" class="tekstfelt">
+          <p>I starten af projektet begyndte jeg med at gennemgå opgavebeskrivelsen og læringsmålene, for at hjælpe med at give mig et overblik. Jeg gennemgik alle emnerne jeg havde været i gennem på første semester og satte dem op imod læringsmålene, så jeg havde hvert læringsmål dækket med et emne. Således havde jeg fået et overblik over opgaven og hvad der skulle med. Jeg opsatte derefter en tidsplan, hvorefter jeg begyndte produktionen af mine tekster. Jeg startede med teksterne for at få gjort det vigtigste først, så jeg altid ville sørge for at have en god tekst hvis siden alligevel ikke blev som ønsket. På den måde endte jeg ikke med en flot side uden godt indhold. Efter teksterne begyndte jeg på en design fase og derefter produktion af selve websitet.</p>
+        </section> <!-- tags_tekst -->
+
+        <section id="tidsplan_billede" class="illustrationer">
+          <img src="billeder/bag_om_siden/tidsplan.PNG" alt="Tidsplan over projektet">
+          <blockquote>
+            <p>Tidsplan fra start til den 20. december. (Der er blevet forsat arbejdet på html og css efter den 20.)</p>
+          </blockquote>
+        </section> <!-- tidsplan_billede -->
+
+        <section id="struktur_overskrift" class="under_overskrift">
+          <h2>Struktur</h2>
+        </section> <!-- struktur_overskrift -->
+
+        <section id="struktur_tekst" class="tekstfelt">
+          <p>En god website skal struktureres godt, og en del af dette er navigationen. For at det var til at finde rundt på min side valgte jeg en navigationsbar foroven, som fleste kender til og der har forventninger til. En af disse forventninger er at hvis man klikker på logoet, vil man komme til forsiden. Jeg har udnyttet denne forventning, og derfor lavet et logo til at nemt kunne finde til forside, samtidig med at binde alle siderne samme. Således er man ikke i tvivl om at man er på samme side. Ligeledes går elementer igen på siderne, som footeren, headeren og selve sidens opbygning. Siderne består alle af en overordnet overskrift, som tydeligt kan ses med sin skrifttype og baggrundsfarve, efterfulgt af en introduktion til emnet samt klikbar oversigt over underemner på siden. Hvert underemnes overskrift er også tydeliggjort med sin farve og skrifttype samt skriftstørrelse. Ved sidens opbygning er der benyttet gestaltlove i form af at overskriften har sin egen farve og underoverskrifterne har en fælles farve. På den måde kan det forstås at underoverskrifterne hænger sammen og overskriften er for sig selv. Samtidig er tekster og billeder for hvert underemne placeret forholdsvis tæt på hinanden, så det tydeligt kan forstås at elementerne hænger sammen og tilhøre underoverskriften. Ligeledes er der mere plads mellem sektionerne med overskrift og indhold. </p>
+          <p>Der er også tænkt informationsarkitektur i forbindelse med navigation og inddeling af emner. Efter alt tekst var blevet produceret, sorterede jeg dem på skrivebordet på min bærbar. Jeg grupperede dem som jeg synes der gav mening, hvor jeg fik en gruppe med ganske få, hvorefter jeg fordelte dem i nye grupper og gav dem hver en overskrift, i forbindelse med en mappe hvori filerne lå. Således lavede jeg en kort lille kortsortering for mig selv. Derefter ville jeg prøve denne sortering af på en der nødvendigvis ikke kender meget til emnerne og derfor ikke en multimediedesigner. Jeg foretog derfor en kortsortering på en bekendt, hvor jeg skrev mine emner ned og klippede dem ud. Jeg gav testpersonen et kort ad gangen hvor han grupperede dem. Derefter gav jeg ham opgaven om at give dem overskrifter. Dette var meget interessant eftersom jeg kunne se at testpersonen forestilling og emnets indhold var anderledes. Derefter foretog jeg en ny kortsortering med samme testperson, hvor personen nu også fik mine overskrifter. Herefter sorterede personen emnerne indenfor de overskrifter jeg havde givet. Testpersonen foretog næsten samme sortering som jeg havde, dog med nogle få undtagelser. Her viste det sig at det var grundet testpersonens opfattelse af emnet, eftersom personen ikke kendte til emnerne. Jeg viste derfor testpersonen min egen sortering, hvor testpersonen derefter fik en bedre forståelse for de emner personen ikke kendte til. Det viste at min sortering kunne forstås også af mennesker der ikke har meget forforståelse for emnerne, og endda hjælpe dem med at forså underemnerne bedre end hvis de var for sig selv.</p>
+        </section> <!-- struktur_tekst -->
+
+        <section id="kortsortering_udenoverskrifter" class="illustrationer">
+          <img src="billeder/bag_om_siden/kortsortering_udenoverskrifter.jpg" alt="Kortsortering uden givet overskrifter">
+          <blockquote>
+              <p>Kortsortering hvor testpersonen selv gav overskrifter </p>
+          </blockquote>
+        </section> <!-- kortsortering_udenoverskrifter -->
+
+        <section id="kortsortering_medoverskrifter" class="illustrationer">
+          <img src="billeder/bag_om_siden/kortsortering_medoverskrifter.jpg" alt="Kortsortering med udgivet overskrifter">
+          <blockquote>
+            <p>Kortsortering hvor testpersonen fik overskrifter. De gule prikker er de emner der er placeret anderledes</p>
+          </blockquote>
+        </section> <!-- kortsortering_medoverskrifter -->
+
+        <section id="design_overskrift" class="under_overskrift">
+          <h2>Design</h2>
+        </section> <!-- design_overskrift -->
+
+        <section id="moodboard_tekst" class="tekstfelt">
+          <p>I begyndelsen af design fasen ville jeg lave et moodboard. Her tog jeg inspiration af forskellige design stile, eftersom jeg inden fasen havde ingen planer eller tanker gjort designet. Jeg kiggede efter billeder med simplistic, minimalistic og rustik design. Da disse tre designtyper er meget udbredt for tiden. Udfra billede jeg fandt og kunne lide med disse nøgleord, satte jeg de billeder til siden som havde en sammenhæng med hinanden. Der var nogle enkelte billeder der nemlig ikke passede ind med de andre. Jeg tog disse billeder og sammensatte mit moodboard som jeg lod mig inspirere af. Hvor den største inspiration har været til baggrunden af siden.</p>
+        </section> <!-- moodboard_tekst -->
+
+        <section id="moodboard_billede" class="illustrationer">
+          <img src="billeder/bag_om_siden/moodboard.png" alt="Moodboard for siden">
+          <blockquote>
+            <p>Moodboardet der blev lavet</p>
+          </blockquote>
+        </section> <!-- moodboard_billede -->
+
+        <section id="styletile_tekst" class="tekstfelt">
+          <p>Jeg tænkte ligeledes meget over farver og har var jeg også inspireret af moodboardet. Jeg vil gerne have siden til at fremstå professionelt så derfor valgte jeg blå som den dominerende farve, og her for at den skulle skille sig ud valgte jeg en mørkere blå til indramning af siderne. Dette er også efter den designstil jeg går efter, som også kan ses på mit moodboard, ikke indgår lyse og stærke fremtrædende farver, men mere nedtonede farver. Derved nedtonede jeg den blå farve samtidig med at den kunne skulle sig ud, ved at gøre den mørkere. Ligeledes valgte jeg blå farve mere som jeg ville bruge til fremhævelse af underemner og kategorier. Farven kan fremtræde som lille, men det er en blå kulør. Her gjorde jeg den bare lysere samt mere mat, for at skille sig ud fra den mørke, samtidig med at have den nedtonede farve der passer stilen. For at ikke have monokrone farve ville jeg have mindst én kulør mere. Her lod jeg mig inspirere af mit moodboard og den mattede brune farve der gik igen. Jeg tog jeg beslutningen for at bruge brun, men brun er ikke en kulør i sig selv. Jeg valgte derfor hurtigt at finde en brun nuance i kuløren orange som er blås komplimentære farve. Ligeledes her besluttede jeg mig for en mørk og en lys så jeg tydeligt kunne lave en kontrast imellem dem, samtidig med at beholde den nedtonede stil. Derved endte jeg med de fire farver. </p>
+          <p>Derudover fandt jeg også tre skrifttyper som passede sammen. Her kiggede jeg på at skrifttyperne ikke var for forskellige, og særligt ved disse omhandlede det ”o” hvor det var tydeligst at det var skrifttyper med lav kontrast. Jeg besluttede mig for tre skrifttyper, en til overemner, underemner og brødtekst, fordi jeg ville have underemnerne i menuen til at have en anden skrifttyper end overemnerne. Jeg satte disse skrifttyper og farverne jeg valgt ind i et style til hvor jeg nemt kunne holder overblik over hvad jeg havde valgt. </p>
+        </section> <!-- styletile_tekst -->
+
+        <section id="styletile_billede" class="illustrationer">
+          <img src="billeder/bag_om_siden/styletile.png" alt="Styletile for siden">
+          <blockquote>
+            <p>Styletile med de valgte farver og skrifttyper</p>
+          </blockquote>
+        </section> <!--styletile_billede -->
+
+        <section id="refleksion_overskrift" class="under_overskrift">
+          <h2>refleksion</h2>
+        </section> <!-- refleksion_overskrift -->
+
+        <section id="refleksion_tekst" class="tekstfelt">
+          <p>Set tilbage på projektet ville jeg havde brugt mere tid på wireframes og idéer med design hertil, da jeg ikke tænkte det med i min tidsplan at der er mange sider at laver wireframes til. Derfor blev det hurtigt og der kom nok ikke lige så mange idéer på bordet som hvis jeg havde taget mig mere tid til det. Jeg ville ligeledes have brugt mere tid og energi på alle billeder og illustrationer, så de størrelsesvis passede på siderne sammen med teksten. Samtidig ville jeg have brugt mere tid på at sætte mig ind i funktioner og elementer i kodningsdelen som jeg ikke kendte så meget til i forvejen, som f.eks. navigationsbaren, pdf-vieweren og slideshow. Er dog nogenlunde tilfreds med det resultat jeg har fået, men alle disse punkter er noget jeg vil have i tankerne i fremtidige projekter. Ligeledes er jeg udmærket klar over at jeg skal læse mine tekster mere grundigt igennem og flere gange. Især efter jeg har lavet design til siden, her vil det være godt at kigge mere grundigt på teksten så den passer bedre til siden og dens layout, og disse ændringer skal ikke foretages når der kodes. Jeg har derved nogle punkter at have med i tankerne fremover og er blevet gjort opmærksom på steder hvor jeg skal planlægge og udføre bedre.</p>
+        </section> <!-- refleksion_tekst -->
+      </div> <!-- pagewrap -->
+
+      <div id="home_button">
+        <a href="index.php"><img src="billeder/home.png" alt="home knap"></a>
+      </div>
+
+      <?php include("footer.inc"); ?>
+    </body> <!-- pagewrap -->
+    <script>
+      function goBack() { /* Går en tilbage i historikken */
+      window.history.back();
+      }
+    </script>
+  </html>
